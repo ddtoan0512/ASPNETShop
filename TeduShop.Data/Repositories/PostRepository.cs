362a55890d36b426efefed1d3ad7b87eb63730af
Toan Dang <ddtoan991205@gmail.com>
2019-11-01 00:13:57 +0700

    commit more service

--- TeduShop.Data/Repositories/PostRepository.cs
+++ TeduShop.Data/Repositories/PostRepository.cs
@@ -9,7 +9,7 @@ namespace TeduShop.Data.Repositories
 {
     public interface IPostRepository : IRepository<Post>
     {
-        //IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow);
+        IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow);
     }
 
     public class PostRepository : RepositoryBase<Post>, IPostRepository
@@ -18,20 +18,20 @@ namespace TeduShop.Data.Repositories
         {
         }
 
-        //public IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow)
-        //{
-        //    var query = from p in DbContext.Posts
-        //                join pt in DbContext.PostTags
-        //                on p.ID equals pt.PostID
-        //                where pt.TagID == tag && p.Status
-        //                orderby p.CreatedDate descending
-        //                select p;
+        public IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow)
+        {
+            var query = from p in DbContext.Posts
+                        join pt in DbContext.PostTags
+                        on p.ID equals pt.PostID
+                        where pt.TagID == tag && p.Status
+                        orderby p.CreatedDate descending
+                        select p;
 
-        //    totalRow = query.Count();
+            totalRow = query.Count();
 
-        //    query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
 
-        //    return query;
-        //}
+            return query;
+        }
     }
 }
