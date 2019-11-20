6ad9b3a6b7779d482e5839ecb644541a37808176
Toan Dang <ddtoan991205@gmail.com>
2019-11-20 22:13:05 +0700

    directive in angularjs

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -3,20 +3,26 @@
 var myApp = angular.module("myModule", []);
 
 myApp.controller("schoolController", schoolController);
-myApp.service('Validator', Validator);
 
-schoolController.$inject = ['$scope', 'Validator'];
+myApp.directive("teduShopDirective", teduShopDirective);
+myApp.service('ValidatorService', ValidatorService);
 
-function schoolController($scope, Validator) {
+
+
+schoolController.$inject = ['$scope', 'ValidatorService'];
+
+function schoolController($scope, ValidatorService) {
     
     $scope.checkNumber = function () {
-        $scope.message = Validator.checkNumber($scope.num);
+        $scope.message = ValidatorService.checkNumber($scope.num);
     }
 
     $scope.num = 1;
+
+
 }
 
-function Validator($window) {
+function ValidatorService($window) {
     return {
         checkNumber: checkNumber
     }
@@ -28,5 +34,12 @@ function Validator($window) {
         else {
             return 'This is odd';
         }
+    }
+}
+
+function teduShopDirective() {
+    return {
+        restrict: "A",
+        templateUrl: "/Scripts/spa/teduShopDirective.html"
     }
 }
